6cb6db8fd92c6918e3c3ebc9486a3c91c21cd7c3
Blake Larson <brlarson87@gmail.com>
2022-02-05 19:52:57 -0600

    update

--- screens/MapScreen.js
+++ screens/MapScreen.js
@@ -1,6 +1,9 @@
-import React, { useState } from 'react';
+import React, { useState, useLayoutEffect, useCallback } from 'react';
 import MapView, { Marker } from 'react-native-maps';
-import { View, Text, StyleSheet } from 'react-native';
+import { TouchableOpacity, Text, StyleSheet, Platform } from 'react-native';
+import Colors from '../constants/Colors';
+
+
 
 const MapScreen = ({navigation}) => {
     const [selectedLocation, setSelectedLocation] = useState();
@@ -10,6 +13,23 @@ const MapScreen = ({navigation}) => {
         latitudeDelta: 0.0922,
         longitudeDelta: 0.0421
     }
+
+    const savePickedLocationHandler = useCallback(() => {
+        if (!selectedLocation) {
+          return;
+        }
+        navigation.navigate("AddPlace", { pickedLocation: selectedLocation });
+      }, [selectedLocation]);
+
+    useLayoutEffect(() => {
+        navigation.setOptions({
+            headerRight: () => (
+                <TouchableOpacity style={styles.headerButton} onPress={savePickedLocationHandler}>
+                    <Text style={styles.headerButtonText}>Save</Text>
+                </TouchableOpacity>
+            )
+        })
+    }, [savePickedLocationHandler]);
 
     const selectLocationHandler = (event) => {
         setSelectedLocation({
@@ -42,6 +62,13 @@ const styles = StyleSheet.create({
     },
     map: {
         flex: 1
+    },
+    headerButton: {
+        marginHorizontal: 20
+    },
+    headerButtonText: {
+        fontSize: 16,
+        color: Platform.OS === 'android' ? '#fff' : Colors.primary
     }
 });
 
